7952c84ba17d8f6e2187907e3dbca0b5c2e73412
mahimamotwani2311 <160347685+mahimamotwani2311@users.noreply.github.com>
2024-06-28 14:47:33 -0700

    Add files via upload

---
+++ SQLQueryfordatanalysis.sql
@@ -0,0 +1,74 @@
+select * from df_orders
+
+-- find top 10 highest revenue generating products
+select top 10 product_id, sum(sale_price) as revenue 
+from df_orders
+group by product_id 
+order by revenue desc;
+ 
+
+
+ -- top 5 highest selling products in each region
+ with cte as (
+ select region,product_id ,sum(sale_price) as revenue from df_orders
+ group by   product_id, region )
+ select * from(
+ select * , ROW_NUMBER()
+ over(partition by region 
+ order by revenue desc) as rn 
+ from cte) A
+ where rn<=5 
+
+ -- find month over month growth comparision for 2022 and 2023 sales eg: jan 2022 vs jan 2023
+ with cte as (
+ select year(order_date) as order_year, month(order_date) as order_month, sum(sale_price) as revenue from df_orders
+ group by year(order_date), month(order_date))
+ select order_month,
+ sum (case when order_year = 2022 then revenue else 0 end) as sales_2022,
+ sum (case when order_year = 2023 then revenue else 0 end) as sales_2023
+ from cte
+ group by order_month
+ order by order_month 
+ 
+
+
+ -- for each category which month has highest sales
+ with cte as (
+ select category, format(order_date,'yyyyMM') as order_year_month,
+ sum(sale_price) as sales 
+ from df_orders
+ group by category, format(order_date,'yyyyMM')
+ )
+ select * from(
+ select *,
+ row_number() over(partition  by category order by sales desc) as rn 
+ from cte) A 
+ where rn =1
+
+
+ -- which sub category has the highest growth in 2023 as compare to 2022
+ with cte as (
+ select region,product_id ,sum(sale_price) as revenue from df_orders
+ group by   product_id, region )
+ select * from(
+ select * , ROW_NUMBER()
+ over(partition by region 
+ order by revenue desc) as rn 
+ from cte) A
+ where rn<=5 */
+
+ -- find which sub category had the highest growth profit in 2023 as compared to 2022
+ with cte as (
+ select year(order_date) as order_year, sub_category, sum(sale_price) as sales from df_orders
+ group by year(order_date), sub_category)
+ ,cte2 as(
+ select sub_category,
+ sum (case when order_year = 2022 then sales else 0 end) as sales_2022,
+ sum (case when order_year = 2023 then sales else 0 end) as sales_2023
+ from cte
+ group by sub_category
+ )
+ select top 1 *,
+  (sales_2023 - sales_2022)*100/sales_2022
+  from cte2 
+  order by (sales_2023 - sales_2022)*100/sales_2022 desc 
